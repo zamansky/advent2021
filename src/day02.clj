7ded771905fe62c98745b167f8b1c66330f2659e
Mike Zamansky <zamansky@gmail.com>
2021-12-02 05:42:38 -0500

    finished part 2

--- src/day02.clj
+++ src/day02.clj
@@ -3,10 +3,32 @@
             [utils :as u]
             [aocd.core :as aocd]
             ))
+
 (def data
   (->> 
-   ;;(slurp "data/sample01.dat")
-   (aocd/input 2021 1)
+   ;;(slurp "data/sample02.dat")
+   (aocd/input 2021 2)
    string/split-lines
-   (map u/parse-int)
+   ;;(map u/parse-int)
+   (map (fn [x] (->>  (string/split x #" ")
+                      (#(let [[a  b] %] [a (u/parse-int b)]))
+                      )))
    ))
+
+(defn solve-part1 [data]
+  (apply *(reduce (fn [[h d] [inst amt] ]
+                    (cond
+                      (= inst "forward") [h (+ d amt)]
+                      (= inst "down") [ (+ h amt) d]
+                      (= inst "up") [ (max 0 (- h amt)) d]
+                      :else [h d])
+                    ) [0 0] data)))
+
+(defn solve-part2 [data]
+  (apply * (drop 1 (reduce (fn [[aim h d] [inst amt] ]
+                             (cond
+                               (= inst "forward") [aim (+ h amt) (+ d (* aim amt))] 
+                               (= inst "down") [(+ aim amt)  h d]
+                               (= inst "up") [(- aim amt) h d]
+                               :else [aim h d])
+                             ) [0 0 0] data))))
