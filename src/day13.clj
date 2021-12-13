35c729c510f39a7b395f60ce2fe5378b3dd6df32
Mike Zamansky <zamansky@gmail.com>
2021-12-13 09:29:57 -0500

    working on 13

--- src/day13.clj
+++ src/day13.clj
@@ -8,26 +8,80 @@
             [clojure.set :as set]))
 (def data
   (->> 
-   ;;(slurp "data/sample11.dat")
-   (aocd/input 2021 11)
-   ( #(string/split % #"\n"))
+   (slurp "data/sample13.dat")
+   ;;(aocd/input 2021 13)
+   ( #(string/split % #"\n\n"))
    ))
 
-(def cols (count (first data)))
-(def rows (count data))
-(def board (into {} (map-indexed
+(def coords (->> (first data)
+                 string/split-lines
+                 (map #(string/split % #","))
 
-                     (fn [row line]
-                       (into {}  (map-indexed (fn [col char]
-                                                {[row col] (-  (int char) 48)} 
-                                                ) line))
-                       )
-                     data ))
-  )
+                 (map (fn [x] (map #(u/parse-int %) x)))
+                 
+                 ))
 
-(defn build-board [state]
-  (str/join "\n"  (for [r  (range rows)]
-                    (str/join "" (for [c (range cols)] (char  (+ 48 (get state [r c])))))
-                    ))
-  )
+(defn board->string [board]
+  (let [maxx (apply max (map first (keys board)))
+        minx (apply min (map first (keys board)))
+        maxy (apply max (map second (keys board)))
+        miny (apply min (map second (keys board)))
+        k (keys board)
+        grid (into [] (repeat (inc maxy) (into [] (repeat (inc maxx) \.))))
+        filled-grid (reduce (fn [b [y x]] (assoc-in b [x y] \#)) grid k)
+        string-grid (map #(apply str %) filled-grid)
+        ]
+    string-grid
+    ))
 
+
+(defn parse-fold-line [line]
+  (let [tmp (string/split line #"=")
+        amt (u/parse-int (second tmp))
+        axis (last (first tmp))
+        ]
+    [axis amt]))
+
+
+(defn new-coord [x y minx miny axis amt]
+  [x y]
+  (if (or
+       (and (= axis \y)
+            (> y amt))
+       (and (= axis \x)
+            (> x amt)))
+    
+    (cond (= axis \y)
+          [x
+           (- y (* 2 (u/abs (- y amt))))
+           ]
+          :else
+          [(- x (* 2 (u/abs (- x amt))))
+           y]
+          )
+    [x y]
+    ))
+
+(defn fold [board line]
+  (let [maxx (apply max (map first (keys board)))
+        minx (apply min (map first (keys board)))
+        maxy (apply max (map second (keys board)))
+        miny (apply min (map second (keys board)))
+        k (keys board)
+        [axis amt] (parse-fold-line line)
+        ]
+    (reduce (fn [b [x y]]
+              (let [ [newx newy] (new-coord x y minx miny axis amt)]
+                (assoc (dissoc b [x y]) [newx newy] \#)))
+            board k)
+    ))
+
+(def board (coords->board coords))
+(def folds (string/split-lines (second data)))
+
+(defn part1 [board folds]
+  (let [newboard (fold board (first folds))]
+    (count (keys newboard))))
+
+(defn part2 [board folds]
+  (reduce (fn [b f] (fold board f)) board folds))
